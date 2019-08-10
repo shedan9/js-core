05123f9c8a4b6037cd74f006c1347986bb9a2af9
shedan <shedan@zenki.ai>
2019-08-10 15:41:59 +0800

    apply and bing

---
+++ function-implement.js
@@ -0,0 +1,34 @@
+// call / bind / new / promise
+
+// call
+Function.prototype._call = function (context, ...arg) {
+  context._tempFn = this;
+  const res = context._tempFn(...arg);
+  Reflect.deleteProperty(context, '_tempFn');
+  return res;
+};
+
+// --test--
+function test(gender, age) {
+  console.log(`${this.name}的性别是${gender}，年龄${age}`);
+}
+const obj = {
+  name: 'sam',
+};
+test._call(obj, '男', 18);
+console.log(Object.prototype.toString._call([]));
+
+// bind
+Function.prototype._bind = function (context) {
+  const fn = this;
+  return function (...arg) {
+    context._tempFn = fn;
+    const res = context._tempFn(...arg);
+    Reflect.deleteProperty(context, '_tempFn');
+    return res;
+  };
+};
+
+// --test--
+const newTest = test._bind(obj);
+newTest('女', 20);
