35b5294253ff729d08e49ae7b821103819d80170
shedan <shedan@zenki.ai>
2019-08-19 10:08:33 +0800

    function arguments optimize

--- function-implement.js
+++ function-implement.js
@@ -2,6 +2,7 @@
 
 // call
 Function.prototype._call = function (context, ...arg) {
+  context = context === undefined ? typeof window === 'undefined' ? global : window : Object(context);
   context._tempFn = this;
   const res = context._tempFn(...arg);
   Reflect.deleteProperty(context, '_tempFn');
@@ -15,11 +16,14 @@ function test(gender, age) {
 const obj = {
   name: 'sam',
 };
-test._call(obj, '男', 18);
+// test._call(obj, '男', 18);
+test.call(1);
+test._call(1);
 console.log(Object.prototype.toString._call([]));
 
 // bind
 Function.prototype._bind = function (context) {
+  context = context === undefined ? typeof window === 'undefined' ? global : window : Object(context);
   const fn = this;
   return function (...arg) {
     context._tempFn = fn;
